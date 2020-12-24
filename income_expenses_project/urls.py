befeb297d75e344987e6e264b9ffa5816483c77d
Armin <armin.pourbeik@gmail.com>
2020-12-24 17:37:32 +0330

    Created api.json endpoint for importing in Postman

--- income_expenses_project/urls.py
+++ income_expenses_project/urls.py
@@ -36,5 +36,10 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path(
+        "api/api.json/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-swagger-without_ui",
+    ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
